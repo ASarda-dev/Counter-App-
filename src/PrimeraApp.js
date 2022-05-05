bf66afe1cde58a6e6d8caaad67478a76af148966
Albert <albertsarda89@gmail.com>
2022-05-05 12:26:38 +0100

    PropTypes.string.isRequired

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,7 +1,7 @@
 import React from "react";
-// import React, { Fragment } from "react";
+import PropTypes from "prop-types";
 
-const PrimeraApp = ({ saludo = "Hola Mundo" }) => {
+const PrimeraApp = ({ saludo }) => {
   return (
     <div>
       <h1>{saludo}</h1>
@@ -11,4 +11,8 @@ const PrimeraApp = ({ saludo = "Hola Mundo" }) => {
   );
 };
 
+PrimeraApp.propType = {
+  saludo: PropTypes.string.isRequired,
+  otra: PropTypes.number,
+};
 export default PrimeraApp;
