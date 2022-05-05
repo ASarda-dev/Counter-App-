ace3e83974f0af64f9b4ac1dc1761ff6fd041c7c
Albert <albertsarda89@gmail.com>
2022-05-05 11:46:51 +0100

    Mostrar por pantalla las props de un objeto

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,7 +1,21 @@
 import React from "react";
+// import React, { Fragment } from "react";
 
 const PrimeraApp = () => {
-  return <h1>Hola Mundo</h1>;
+  const saludo = {
+    nombre: "Albert",
+    edad: 32,
+    ciudad: "Barcelona",
+  };
+  return (
+    <div>
+      <h1>
+        Hola {saludo.nombre} tu edad es de {saludo.edad} vives en{" "}
+        {saludo.ciudad}
+      </h1>
+      <p>Mi primera App</p>
+    </div>
+  );
 };
 
 export default PrimeraApp;
